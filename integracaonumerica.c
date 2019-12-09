788245eb67414bfda4a862100637e2126f839ad4
alison.almeida <wsalisonxp@gmail.com>
2019-12-08 21:24:52 -0400

    Corrigindo bugs

--- integracaonumerica.c
+++ integracaonumerica.c
@@ -1,83 +1,117 @@
 #include <stdio.h>
 #include <math.h>
 
-double funcao(double _passo, int _i){
-    return 1 / (1 + pow((_passo * _i), 2));
+double funcao(double x){
+    return sin(x);
 }
 
 double Trapezio(double lsuperior, double linferior, int iteracoes){
     double passo;
-    double resultado[iteracoes], integration, somatorio;
+    double resultado[iteracoes], somatorio, sum;
+    int _iteracoes;
+    double x;
     int i;
 
+    somatorio = 0;
+    _iteracoes = 0;
     passo = (lsuperior - linferior) / iteracoes;
-
+    printf("passo: %f\n", passo);
+    x = linferior;
+    
+    //Calcula todos os pequenos intervalos da funcao
     for(i = 0; i <= iteracoes; i++){
-        resultado[i] = funcao(passo, i);
+        resultado[_iteracoes] = funcao(x);
+        printf("resultado:  %f, x: %f, i: x %d\n", funcao(x), x, i);
+        _iteracoes++;
+        x = x + passo;
     }
     
-    somatorio = resultado[0] + resultado[iteracoes];
+    //soma os extremos
+    sum = resultado[0] + resultado[iteracoes];
 
     for(i = 1; i < iteracoes; i++){
-        somatorio = somatorio + (resultado[i] * 2);
+        somatorio = somatorio + resultado[i];
     }
-    
-    integration = (passo / 2) * somatorio;
-    return integration;
+
+    return (passo / 2) * (sum + (2 * somatorio));
 }
 
 double Simpson1Regra(double lsuperior, double linferior, int iteracoes){
     double passo;
-    double resultado[iteracoes], integration, somatorio;
+    double resultado[iteracoes], somatorio, sum, sumpar, sumimpar;
+    int _iteracoes;
+    double x;
     int i;
 
+    somatorio = 0;
+    sumpar = 0;
+    sumimpar = 0;
+    
+    _iteracoes = 0;
     passo = (lsuperior - linferior) / iteracoes;
-
+    printf("passo: %f\n", passo);
+    x = linferior;
+    
+    //Calcula todos os pequenos intervalos da funcao
     for(i = 0; i <= iteracoes; i++){
-        resultado[i] = funcao(passo, i);
+        resultado[_iteracoes] = funcao(x);
+        printf("resultado:  %f, x: %f, i: x %d\n", funcao(x), x, i);
+        _iteracoes++;
+        x = x + passo;
     }
     
-    somatorio = resultado[0] + resultado[iteracoes];
+    //soma os extremos
+    sum = resultado[0] + resultado[iteracoes];
 
     for(i = 1; i < iteracoes; i++){
         if(i%2 == 0){
-            somatorio = somatorio + (resultado[i] * 2);
+            sumpar = sumpar + (resultado[i] * 2);
         }
         else{
-            somatorio = somatorio + (resultado[i] * 4);
+            sumimpar = sumimpar + (resultado[i] * 4);
         }
     }
-    
-    integration = (passo / 3) * somatorio;
-    return integration;
+    return (passo/3) * (sum + sumpar+ sumimpar);
 }
 
 double Simpson2Regra(double lsuperior, double linferior, int iteracoes){
     double passo;
-    double resultado[iteracoes], integration, somatorio;
+    double resultado[iteracoes], somatorio, sum, sumpar, sumimpar;
+    int _iteracoes;
+    double x;
     int i;
 
+    somatorio = 0;
+    sumpar = 0;
+    sumimpar = 0;
+    
+    _iteracoes = 0;
     passo = (lsuperior - linferior) / iteracoes;
-
+    printf("passo: %f\n", passo);
+    x = linferior;
+    
+    //Calcula todos os pequenos intervalos da funcao
     for(i = 0; i <= iteracoes; i++){
-        resultado[i] = funcao(passo, i);
+        resultado[_iteracoes] = funcao(x);
+        printf("resultado:  %f, x: %f, i: x %d\n", funcao(x), x, i);
+        _iteracoes++;
+        x = x + passo;
     }
     
-    somatorio = resultado[0] + resultado[iteracoes];
+    //soma os extremos
+    sum = resultado[0] + resultado[iteracoes];
 
     for(i = 1; i < iteracoes; i++){
         if(i%3 == 0){
-            somatorio = somatorio + (resultado[i] * 2);
+            sumpar = sumpar + (resultado[i] * 2);
         }
         else{
-            somatorio = somatorio + (resultado[i] * 3);
+            sumimpar = sumimpar + (resultado[i] * 3);
         }
     }
-
-    integration = ((passo * 3) / 8) * somatorio;
-    return integration;
+    return ((passo * 3 ) /8) * (sum + sumpar + sumimpar);
 }
 
 int main(){
-    printf("%f\n", Trapezio(1, 0, 4));
+    printf("%f\n", Simpson2Regra(3.14, 0, 20));
 }
